105c895faae714fdcdb55588a236c75b2a5731ba
amirsh <nomadcodr88@gmail.com>
2023-09-26 21:18:27 +0300

    Finished project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::error::Error;
+
+use csv;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./test.csv") {
+        eprintln!("{}", e);
+    }
 }
